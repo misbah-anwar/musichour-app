0b88203cf18346c348af0a0832da5ace1f11e2d5
misbah-anwar <misbahanwar1802@gmail.com>
2023-10-18 12:58:08 +0530

    first version

--- lib/screens/getmusicpage.dart
+++ lib/screens/getmusicpage.dart
@@ -1,42 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class GetMusicPage extends StatefulWidget {
   final SharedPreferences prefs;
+  final int familyId;
 
-  GetMusicPage({required this.prefs, required int familyId});
+  GetMusicPage({required this.prefs, required this.familyId});
 
   @override
   _GetMusicPageState createState() => _GetMusicPageState();
 }
 
 class _GetMusicPageState extends State<GetMusicPage> {
-  Future<void> _fetchMusicDetails() async {
-    try {
-      var response = await http.get(
-        Uri.parse(
-            'http://baatcheet1-env.eba-3uzrj2rz.us-east-2.elasticbeanstalk.com/getMusicContentByFamilyId/41'),
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-        },
-      );
+  late Future<String> videoUrl;
 
-      if (response.statusCode == 200) {
-        // Successful response
-        print('Fetched music details successfully');
+  @override
+  void initState() {
+    super.initState();
+    videoUrl = fetchVideoUrl();
+  }
 
-        // Convert the response to a JSON object
-        var jsonData = jsonDecode(response.body);
-        print(jsonData);
-        await _fetchMusicDetails();
+  Future<String> fetchVideoUrl() async {
+    final response = await http.get(
+      Uri.parse(
+          'http://baatcheet1-env.eba-3uzrj2rz.us-east-2.elasticbeanstalk.com/getMusicContentByFamilyId/${widget.familyId}'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      var jsonData = jsonDecode(response.body);
+      if (jsonData is List && jsonData.isNotEmpty) {
+        var firstItem = jsonData.first;
+        if (firstItem.containsKey('music_url')) {
+          return firstItem['music_url'];
+        } else {
+          throw Exception('music_url not found in response');
+        }
       } else {
-        // Handle errors here
-        print('Failed to fetch music details');
+        throw Exception('Empty or invalid response from the API');
       }
-    } catch (error) {
-      print('Error fetching music details: $error');
+    } else {
+      throw Exception('Failed to load video URL');
     }
   }
 
@@ -45,6 +54,31 @@ class _GetMusicPageState extends State<GetMusicPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Get Music'),
+      ),
+      body: FutureBuilder<String>(
+        future: videoUrl,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            print('Video URL received: ${snapshot.data}');
+            return Center(
+              child: YoutubePlayer(
+                controller: YoutubePlayerController(
+                  initialVideoId: YoutubePlayer.convertUrlToId(snapshot.data!)!,
+                  flags: YoutubePlayerFlags(
+                    autoPlay: true,
+                    mute: false,
+                  ),
+                ),
+                showVideoProgressIndicator: true,
+                progressIndicatorColor: Colors.blueAccent,
+              ),
+            );
+          }
+        },
       ),
     );
   }
